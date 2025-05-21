a86510ff1e4fc5e6443b9b856e7c98fe8d2b497c
allangrm <allanguilherme.ag70@gmail.com>
2025-05-21 12:10:21 -0300

    Adição de branch, Queen

--- src/main/java/com/seios/warofkings/pieces/types/Queen.java
+++ src/main/java/com/seios/warofkings/pieces/types/Queen.java
@@ -1,4 +1,60 @@
 package com.seios.warofkings.pieces.types;
 
-public class Queen {
+import com.seios.warofkings.pieces.ChessPiece;
+import com.seios.warofkings.pieces.enums.Type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queen extends ChessPiece {
+
+    public Queen(int position, Type type){
+        if (!(type.getValor() == 4) && !(type.getValor() == 10)) {
+            throw new IllegalArgumentException("");
+        }
+        super.position =position;
+        super.type = type;
+        super.n_moves = 0;
+    }
+
+    @Override
+    public List<Integer> getPossibleMoves() {
+        List<Integer> possibilities = new ArrayList<>();
+
+
+        while (true) { // verificacao linda, enquanto a posicao existir E nao estiver ocupada
+            possibilities.add(getX() - 1, getY());
+        }
+
+        while(true){
+            possibilities.add(getX() + 1, getY());
+        }
+
+        while (true){
+            possibilities.add(getX(), getY() - 1);
+        }
+
+        while (true){
+            possibilities.add(getX(), getY() + 1);
+
+        }
+
+        while (true){
+            possibilities.add(getX() - 1, getY() - 1);
+        }
+
+        while (true){
+            possibilities.add(getX() - 1, getY() + 1);
+
+        }
+
+        while (true){
+            possibilities.add(getX() + 1, getY() + 1);
+
+        }
+
+        while (true){
+            possibilities.add(getX() + 1, getY() - 1);
+        }
+    }
 }
