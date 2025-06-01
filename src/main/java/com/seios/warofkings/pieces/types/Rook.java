1a38e69a2bf6d6b1deb19b9759200a10705313f3
beatriz <beatrizmm2610@gmail.com>
2025-06-01 19:26:42 -0300

    tem nada pronto to so subindo pq mandaram

--- src/main/java/com/seios/warofkings/pieces/types/Rook.java
+++ src/main/java/com/seios/warofkings/pieces/types/Rook.java
@@ -13,14 +13,32 @@ import com.seios.warofkings.pieces.enums.Type;
  * @since YYYY-MM-DD
  */
 public class Rook extends ChessPiece {
+
+    private Rook(int position, Type type){
+        super();
+        this.position =position;
+        this.type = type;
+
+    }
+
+    public Rook(int position, Type type, int n_moves){
+        if (!(type.getValor() == 4) && !(type.getValor() == 10)) {
     public Rook(int position, Type type, int n_moves) throws IllegalArgumentException {
         if (!(type.getValor() == 1) && !(type.getValor() == 7)) {
             throw new IllegalArgumentException("");
         }
-
-        this.position = position;
+        this.position =position;
         this.type = type;
+        this.n_moves = n_moves;
+
         this.n_moves = 0;
+    }
+
+    public static Rook createRook(int position, Type type) {
+        if (!(type.getValor() == 4) && !(type.getValor() == 10)) {
+            throw new IllegalArgumentException("");
+        }
+        return new Rook(position, type);
     }
 
     /**
@@ -34,26 +52,41 @@ public class Rook extends ChessPiece {
      */
     @Override
     public List<Integer> getPossibleMoves(ChessPiece[][] board) {
-        List<Integer> possibilities = new ArrayList<>();
+            List<Integer> possibleMoves = new ArrayList<>();
+            int pos = this.position;
 
-        int x = getX();
-        int y = getY();
+            int forward = -10;
+            int backward = 10;
 
-        for (int col = 0; col < 8; col++) {
-            if (col != x) {
-                possibilities.add((col * 10) + y);
-            }
-        }
+            int right = 1;
+            int left = -1;
+    }
 
-        for (int row = 0; row < 8; row++) {
-            if (row != y) {
-                possibilities.add((x * 10) + row);
-            }
-        }
+
+    //@Override
+    //public List<Integer> getPossibleMoves(ChessPiece[][] board) {
+      //  List<Integer> possibilities = new ArrayList<>();
+
+        //int x = getX();
+      //  int y = getY();
+
+        //for (int col = 0; col < 8; col++) {
+          //  if (col != x) {
+            //    possibilities.add((col * 10) + y);
+            //}
+        //}
+
+        //for (int row = 0; row < 8; row++) {
+          //  if (row != y) {
+            //    possibilities.add((x * 10) + row);
+            //}
+        //}
 
         // tem que fazer a verificacao ainda de se tem peça e se deixa o meu rei em xeque p poder andar ou não.
             // seria o uso do kingCheck?
 
+        //return possibilities;
+    //}
         return possibilities;
     }
 
