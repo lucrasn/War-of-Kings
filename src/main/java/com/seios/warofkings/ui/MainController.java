6969393b391592f983a12bf25783fe9769ef35ef
lucrasn <lucrasnobrega.araujo@gmail.com>
2025-06-14 16:33:52 -0300

    movingPieces

--- src/main/java/com/seios/warofkings/ui/MainController.java
+++ src/main/java/com/seios/warofkings/ui/MainController.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.seios.warofkings.board.Board;
@@ -88,28 +89,41 @@ public class MainController {
     private ImageView clickedPiece;
 
     @FXML
-    public void movingPieces(){
-        for(Node node : pecas.getChildren()) {
-            if(node instanceof ImageView) {
+    public void movingPieces() {
+        final ChessPiece[] selectedPiece = {null};
+        final ImageView[] selectedImage = {null};
+        final List<Integer>[] possibleMoves = new List[]{null};
+
+        for (Node node : pecas.getChildren()) {
+            if (node instanceof ImageView imageView) {
                 node.setOnMouseClicked(event -> {
-                    ImageView clickedImg = (ImageView) node;
+                    Integer columnY = GridPane.getColumnIndex(imageView);
+                    Integer rowX = GridPane.getRowIndex(imageView);
+                    int positionTo = rowX * 10 + columnY;
 
-                    System.out.println("VOCE CLICOU");
-                    Integer pieceColumnY = GridPane.getColumnIndex(clickedImg);
-                    Integer pieceRowX = GridPane.getRowIndex(clickedImg);
-                    ChessPiece pieceClicked = board.getPieces()[pieceRowX][pieceColumnY];
-                    List<Integer> movesPiece = pieceClicked.getPossibleMoves(board.getPieces());
+                    if (selectedPiece[0] == null) {
+                        ChessPiece piece = board.getPieces()[rowX][columnY];
+                        if (piece != null) {
+                            selectedPiece[0] = piece;
+                            selectedImage[0] = imageView;
+                            possibleMoves[0] = piece.getPossibleMoves(board.getPieces());
+                            System.out.println("Peça selecionada: " + piece);
+                            System.out.println("Movimentos possíveis: " + possibleMoves[0]);
+                        }
+                    } else {
+                        boolean moved = selectedPiece[0].moveTo(positionTo, possibleMoves[0], board);
+                        if (moved) {
+                            pecas.getChildren().remove(selectedImage[0]);
+                            pecas.add(selectedImage[0], columnY, rowX);
+                            // atualizar o tabuleiro
+                            System.out.println("Peça movida para " + positionTo);
+                        } else {
+                            System.out.println("Movimento inválido.");
+                        }
 
-                    if (true) { //
-                        System.out.println("LUGAR SELECIONADO");
-
-                        Integer columnY = GridPane.getColumnIndex(clickedImg);
-                        Integer rowX = GridPane.getRowIndex(clickedImg);
-
-                        pecas.getChildren().remove(clickedImg);
-                        pecas.add(clickedImg, columnY, rowX);
-
-                        creatingPieces();
+                        selectedPiece[0] = null;
+                        selectedImage[0] = null;
+                        possibleMoves[0] = null;
                     }
                 });
             }
@@ -119,6 +133,4 @@ public class MainController {
 
         //    }
     }
-
-
 }
