fe0279ad5cfbc9f130bad4f52793376e9e5fbf28
beatriz <beatrizmm2610@gmail.com>
2025-06-14 19:57:55 -0300

    roque i guess

--- src/main/java/com/seios/warofkings/pieces/types/King.java
+++ src/main/java/com/seios/warofkings/pieces/types/King.java
@@ -2,9 +2,11 @@ package com.seios.warofkings.pieces.types;
 
 import com.seios.warofkings.pieces.ChessPiece;
 import com.seios.warofkings.pieces.enums.Type;
+import com.seios.warofkings.utils.BoardUtils;
+import com.seios.warofkings.utils.PieceUtils;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Esta classe é responsável pela implementação dos movimentos da peça Rei
@@ -15,6 +17,13 @@ import java.util.Map;
  */
 public class King extends ChessPiece {
     boolean roque = false; // true: listmoves tem roque. false: n tem roque
+
+    public boolean isWhite() {
+
+        return this.type.getValor() <= 6;   // brancas até 6 pretas depois de 7
+
+
+    }
 
     public King(int position, Type type, int n_moves) {
         if (!(type.getValor() == 5) && !(type.getValor() == 11)) {
@@ -41,33 +50,96 @@ public class King extends ChessPiece {
 
     @Override
     public List<Integer> getPossibleMoves(ChessPiece[][] board) {
-        // List<ChessPiece> rooks = BoardUtils.finsPieces(isWhite ? Type.ROOK_WHITE : Type.ROOK_BLACK);
-        // if (rooks.getFist.getNMoves == 0 && this.getNMoves == 0) {
-        // roque = true;
-        // ...
-        // }
 
-        return List.of();
-    }
 
+<<<<<<< Updated upstream
     // Faltou isso
     //@Override
     public boolean moveTo(int position, List<Integer> listMoves, ChessPiece[][] board) {
+=======
+        int pos = this.position;
+
+        int forward = -10;
+        int backward = 10;
+
+        int right = 1;
+        int left = -1;
+
+        int northwest = -11;
+        int southwest = 9;
+
+        int northeast = -9;
+        int southeast = 11;
+        List<Integer> possibleMoves = new ArrayList<Integer>();
+
+
+        List<ChessPiece> rooks = BoardUtils.findPieces(board, isWhite() ? Type.ROOK_WHITE : Type.ROOK_BLACK);
+
+        ChessPiece rook = rooks.get(0); // o getFirt n é um metodo d lista
+        if (rook.getN_moves() == 0 && this.getN_moves() == 0) { // torrei e rei sem movimentos
+            int kingPos = this.getPosition();
+            int rookPos = rook.getPosition();
+
+            int direction;
+            if (rookPos > kingPos) {
+                direction = 1; // roque da direita
+            } else {
+                direction = -1; // roque da esquerda
+            }
+            int roqueDireira = kingPos + direction;
+            int roqueEsquerda = kingPos + 2 * direction;
+
+
+            //if(board[PieceUtils.getX(roqueDireira)][PieceUtils.getY(roqueDireira)] == null ||
+            //          board[PieceUtils.getX(roqueEsquerda)][PieceUtils.getY(roqueEsquerda)] == null) {
+            //if (!kingCheck(roqueEsquerda, board) || !kingCheck(roqueDireira, board) ) {
+            //  roque = true;
+
+            //}
+            //}
+            if (board[PieceUtils.getX(roqueDireira)][PieceUtils.getY(roqueDireira)] == null) {
+                if (!kingCheck(roqueDireira, board)) {
+                    roque = true;
+                    possibleMoves.add(roqueDireira);
+                }
+            }
+
+            if (board[PieceUtils.getX(roqueEsquerda)][PieceUtils.getY(roqueEsquerda)] == null) {
+                if (!kingCheck(roqueEsquerda, board)) {
+                    roque = true;
+                    possibleMoves.add(roqueEsquerda);
+                }
+            }
+
+        }
+        return possibleMoves;
+
+
+        // Faltou isso
+        //@Override
+        //public boolean moveTo(int position, List<Integer> listMoves, ChessPiece[][] board) {
+>>>>>>> Stashed changes
         // List<ChessPiece> rooks = BoardUtils.finsPieces(isWhite ? Type.ROOK_WHITE : Type.ROOK_BLACK);
         // listMoves.in(position) -> true
         // roque == true
         // List<Integer> ED = new ArrayList<>() {{add(rooks.getIndexLast()) /n add(rooks.getIndexAntepenultimo())}};
         // listMoves.index(position) -> x
         // ED.in(x):
-            // if (x == ED.getFist()) {
-                // this.setPosition(position)
-                // rooks.getLast().setPostion(position + 1)
-            // } else {
-                // this.setPosition(position)
-                // rooks.getLast().setPostion(position - 1)
-            // }
+        // if (x == ED.getFist()) {
+        // this.setPosition(position)
+        // rooks.getLast().setPostion(position + 1)
+        // } else {
+        // this.setPosition(position)
+        // rooks.getLast().setPostion(position - 1)
+        // }
 
         //TODO: verificações como: Se é possível o movimento de acordo com o andar da peça (essa verificação fica para as classes concretas); Se não tem uma peça do mesmo exercíto nessa posição; etc
+<<<<<<< Updated upstream
         return true; //super.moveTo(position, listMoves, board);
     }
 }
+=======
+        //  return super.moveTo(position, listMoves, board);
+        //}
+    }}
+>>>>>>> Stashed changes
