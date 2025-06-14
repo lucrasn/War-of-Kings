4232de7b83ff2d4e6c1bea8280f117e74f217124
lucrasn <lucrasnobrega.araujo@gmail.com>
2025-06-14 17:28:44 -0300

    Corrigindo o aviso

--- src/main/java/com/seios/warofkings/ui/MainController.java
+++ src/main/java/com/seios/warofkings/ui/MainController.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.Region;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 import com.seios.warofkings.board.Board;
 import com.seios.warofkings.pieces.ChessPiece;
@@ -86,13 +87,11 @@ public class MainController {
         }
     }
 
-    private ImageView clickedPiece;
-
     @FXML
     public void movingPieces() {
-        final ChessPiece[] selectedPiece = {null};
-        final ImageView[] selectedImage = {null};
-        final List<Integer>[] possibleMoves = new List[]{null};
+        AtomicReference<ChessPiece> selectedPiece = new AtomicReference<ChessPiece>();
+        AtomicReference<ImageView> selectedImage = new AtomicReference<ImageView>();
+        AtomicReference<List<Integer>> possibleMoves = new AtomicReference<List<Integer>>();
 
         for (Node node : pecas.getChildren()) {
             if (node instanceof ImageView imageView) {
@@ -101,29 +100,29 @@ public class MainController {
                     Integer rowX = GridPane.getRowIndex(imageView);
                     int positionTo = rowX * 10 + columnY;
 
-                    if (selectedPiece[0] == null) {
+                    if (selectedPiece.get() == null) {
                         ChessPiece piece = board.getPieces()[rowX][columnY];
                         if (piece != null) {
-                            selectedPiece[0] = piece;
-                            selectedImage[0] = imageView;
-                            possibleMoves[0] = piece.getPossibleMoves(board.getPieces());
-                            System.out.println("Peça selecionada: " + piece);
-                            System.out.println("Movimentos possíveis: " + possibleMoves[0]);
+                            selectedPiece.set(piece);
+                            selectedImage.set(imageView);
+                            possibleMoves.set(piece.getPossibleMoves(board.getPieces()));
+                            System.out.println("Peca selecionada: " + piece);
+                            System.out.println("Movimentos possiveis: " + possibleMoves.get());
                         }
                     } else {
-                        boolean moved = selectedPiece[0].moveTo(positionTo, possibleMoves[0], board);
+                        boolean moved = selectedPiece.get().moveTo(positionTo, possibleMoves.get(), board);
                         if (moved) {
-                            pecas.getChildren().remove(selectedImage[0]);
-                            pecas.add(selectedImage[0], columnY, rowX);
-                            // atualizar o tabuleiro
+                            pecas.getChildren().remove(selectedImage.get());
+                            pecas.add(selectedImage.get(), columnY, rowX);
+                            creatingPieces(); // se precisar redesenhar o estado das peças
                             System.out.println("Peça movida para " + positionTo);
                         } else {
                             System.out.println("Movimento inválido.");
                         }
 
-                        selectedPiece[0] = null;
-                        selectedImage[0] = null;
-                        possibleMoves[0] = null;
+                        selectedPiece.set(null);
+                        selectedImage.set(null);
+                        possibleMoves.set(null);
                     }
                 });
             }
